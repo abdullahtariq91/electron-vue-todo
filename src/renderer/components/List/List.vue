c86d01537deca5b345ce39eff51342041dc20517
Abdullah Tariq <abdullahtariq91@gmail.com>
2018-02-21 00:25:14 +0500

    Testing changes  2

--- src/renderer/components/List/List.vue
+++ src/renderer/components/List/List.vue
@@ -6,8 +6,8 @@
     <v-divider></v-divider>
     <v-layout row wrap class="pa-3">
       <v-flex xs12 sm6 md4 class="item">
-        <div class="name">Path:</div>
-        <div class="value">\{{ path }}</div>
+        <div class="name">Random</div>
+        <div class="value"></div>
       </v-flex>
     </v-layout>
   </v-card>
